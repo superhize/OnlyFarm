257c538a98337facab29244506998173e1b7cfeb
superhize <superhize@gmail.com>
2024-02-14 05:01:13 +0100

    Bump version

--- build.gradle.kts
+++ build.gradle.kts
@@ -15,7 +15,7 @@ plugins {
 
 val baseGroup: String by project
 val mcVersion: String by project
-version = "0.0.3"
+version = "0.0.4"
 group = "be.hize"
 val mixinGroup = "$baseGroup.mixin"
 val modid: String by project
